a0bed6679f1353e4e9193c7f5b2ec2a9ca23cc91
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-09 12:26:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+      array = [0,1,2,3]
+  array.pop()
 end
 
 def remove_element_from_start_of_array(array)
