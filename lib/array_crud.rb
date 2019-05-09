d9e61c2b7d4843c3fe303313cd342eadbf513458
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-09 12:25:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,8 @@ end
 
 def add_element_to_end_of_array(array, element)
   array = [0,1,2,3]
-  array.push(4)
+  element = "arrays!"
+  array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
