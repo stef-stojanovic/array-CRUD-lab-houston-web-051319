15444d7e1a13c16e60f81cc515c2f3d907a70ab0
Stefan Stojanovic <stefan.stojanovic2202@gmail.com>
2019-05-09 12:25:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+    array = [0,1,2,3]
+  element = "first"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
